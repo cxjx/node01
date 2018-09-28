c048056014310a1c18136e96ac918606db42e44d
cxjx <648932803@qq.com>
2018-09-28 09:34:11 +0800

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
-const pgp = require('pg-promise')({
-  // Initialization Options
-  capSQL: true // capitalize all generated SQL
-});
+// const pgp = require('pg-promise')({
+//   // Initialization Options
+//   capSQL: true // capitalize all generated SQL
+// });
+
+const URL_TABLE_NAME = 'url';
+const IMAGE_TABLE_NAME = 'image';
 
 module.exports = {
   /* https://github.com/website-scraper/node-website-scraper/blob/master/lib/config/defaults.js */
@@ -61,31 +64,31 @@ module.exports = {
   dbConnection: 'postgresql://postgres:root123@localhost:5432/test',
   // our set of columns, to be created only once, and then shared/reused,
   // to let it cache up its formatting templates for high performance:
-  ColumnSet_domain: new pgp.helpers.ColumnSet(['name'], {table: 'domain'}),
-  ColumnSet_analysis: new pgp.helpers.ColumnSet([
-    'imgurl',
-    'MotionBlur',
-    'Light',
-    'ColorHarmony',
-    'Symmetry',
-    'VividColor',
-    'Repetition',
-    'Content',
-    'DoF',
-    'Object',
-    'RuleOfThirds',
-    'BalancingElement',
-    'score',
-  ], {table: 'analysis'}),
-  SQL_CREATE_TABLE_DOMAIN: 'CREATE TABLE IF NOT EXISTS "domain" \
+  // ColumnSet_domain: new pgp.helpers.ColumnSet(['name'], {table: URL_TABLE_NAME}),
+  // ColumnSet_analysis: new pgp.helpers.ColumnSet([
+  //   'imgurl',
+  //   'MotionBlur',
+  //   'Light',
+  //   'ColorHarmony',
+  //   'Symmetry',
+  //   'VividColor',
+  //   'Repetition',
+  //   'Content',
+  //   'DoF',
+  //   'Object',
+  //   'RuleOfThirds',
+  //   'BalancingElement',
+  //   'score',
+  // ], {table: IMAGE_TABLE_NAME}),
+  SQL_CREATE_TABLE_DOMAIN: 'CREATE TABLE IF NOT EXISTS "'+URL_TABLE_NAME+'" \
     (\
       "id" SERIAL PRIMARY KEY, \
       "name" varchar(64) UNIQUE NOT NULL\
     )',
-  SQL_CREATE_TABLE_ANALYSIS: 'CREATE TABLE IF NOT EXISTS "analysis" \
+  SQL_CREATE_TABLE_ANALYSIS: 'CREATE TABLE IF NOT EXISTS "'+IMAGE_TABLE_NAME+'" \
     (\
       "id" SERIAL PRIMARY KEY, \
-      "domainid" int REFERENCES "domain"("id"), \
+      "urlid" int REFERENCES "'+URL_TABLE_NAME+'"("id"), \
       "imgurl" varchar(512) UNIQUE NOT NULL, \
       "MotionBlur" real, \
       "Light" real, \
@@ -100,7 +103,7 @@ module.exports = {
       "BalancingElement" real, \
       "score" real\
     )',
-  SQL_SELECT_FROM_DOMAIN: 'select name from domain',
-  SQL_INSERT_INTO_DOMAIN: 'insert into domain(${this:name}) values(${this:csv}) on conflict(name) do nothing;',
-  SQL_INSERT_INTO_ANALYSIS: 'insert into analysis(${this:name}) values(${this:csv}) on conflict(imgurl) do nothing;',
+  SQL_SELECT_FROM_DOMAIN: 'select name from '+URL_TABLE_NAME,
+  SQL_INSERT_INTO_DOMAIN: 'insert into '+URL_TABLE_NAME+'(${this:name}) values(${this:csv}) on conflict(name) do nothing;',
+  SQL_INSERT_INTO_ANALYSIS: 'insert into '+IMAGE_TABLE_NAME+'(${this:name}) values(${this:csv}) on conflict(imgurl) do nothing;',
 }
